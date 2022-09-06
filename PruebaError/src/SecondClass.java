c3fb733d0bbeefb75bf397b94a1667c6b5e02b30
AndreRH09 <aanazcoh@unsa.edu.pe>
2022-09-05 22:19:37 -0500

    new project

---
+++ PruebaError/src/SecondClass.java
@@ -0,0 +1,12 @@
+public class SecondClass {
+    private int Integernuevo;
+
+    public static void main(String[] args) {
+
+
+        SecondClass obj = new SecondClass();
+        obj.Integernuevo = 12;
+
+        System.out.println(obj.Integernuevo);
+    }
+}
